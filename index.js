4bfd62c4f5291ecd160f05be1a977348a23a6f8b
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2024-12-17 03:02:52 -0300

    Atualizei

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const express = require("express")
-const axios = require("axios")
-const cron = require("node-cron")
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
 
-const app = express()
-const PORT = process.env.PORT || 8081
+const app = express();
+const PORT = process.env.PORT || 8081;
 
-cron.schedule('*/5 * * * *', async () => {
+// Variável para armazenar o contador
+let requestCount = 0;
+
+// Tarefa agendada com cron para realizar o ping a cada minuto
+cron.schedule("*/1 * * * *", async () => {
     try {
-      await axios.get('https://wppchatbotfe.onrender.com/ping');
-      console.log("reqisição bem sucedida")
-      console.log('Ping bem-sucedido!');
+        await axios.get("https://wppchatbotfe.onrender.com/ping");
+        requestCount++; // Incrementa o contador a cada requisição bem-sucedida
+        console.log("Requisição bem-sucedida");
     } catch (error) {
-      console.error('Erro ao fazer o ping:', error);
+        console.error("Erro ao fazer o ping:", error);
     }
 });
 
-app.get("/", (req, res)=>{
-  res.send("aplicação rodandando")
-})
+// Rota GET para exibir a quantidade de requisições
+app.get("/", (req, res) => {
+    res.send(`Aplicação rodando. Total de requisições bem-sucedidas: ${requestCount}`);
+    console.log(requestCount)
+});
 
-
-app.listen(PORT, ()=>{
-    console.log(`servidor rodando na porta: ${PORT}`)
-})
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`);
+});
